ff306c88dbf657c92a83d0fa01f3e5e37f3fc964
shinarit <shinarit@nng.com>
2014-04-23 14:38:14 +0200

    line validator added

--- sudocube.py
+++ sudocube.py
@@ -17,6 +17,7 @@ class SudoCube:
     dimensions: the orthotope that is the basic building stone of the sudo(hyper)cube, everything comes from this"""
 
     self.dimensions = map(int, dimensions)
+    self.numOfDimensions = len(self.dimensions)
     self.edgeSize = reduce(mul, self.dimensions)
     self.unitSizes = map(lambda x: self.edgeSize ** x, range(len(self.dimensions) + 1))
     
@@ -56,8 +57,42 @@ class SudoCube:
     if 2 == len(self.dimensions):
       self.printLayer(0)
 
+  def validatePosition(self, index):
+    "startpointokból indulunk self.dimension[i]-nyi stepekkel haladva"
+    coords = self.innerToCoordinate(index)
+    startPoints = [self.coordinateToInner(*(coords[:i] + [0] + coords[i + 1:])) for i in xrange(self.numOfDimensions)]
+    for start, step in izip(startPoints, self.unitSizes):
+      checker = [0] * (self.edgeSize)
+      for value in self.table[start : start + self.edgeSize * step : step]:
+        if value > 0:
+          checker[value - 1] += 1
+          if checker[value - 1] > 1:
+            return False
+    return True
+
+  def increaseOnPosition(self, index):
+    self.table[index] += 1
+    while self.table[index] <= self.edgeSize and not self.validatePosition(index):
+      self.table[index] += 1
+    return self.table[index] <= self.edgeSize
+
+  def solve(self):
+    emptyPlaces = [x[1] for x in izip(self.table, xrange(len(self.table))) if 0 == x[0]]
+    if 0 == len(emptyPlaces):
+      return
+    curr = 0
+    while curr >= 0 and curr < len(emptyPlaces):
+      index = emptyPlaces[curr]
+      if self.increaseOnPosition(index):
+        curr += 1
+      else:
+        self.table[index] = 0
+        curr -= 1
+
 
 if __name__ == "__main__":
-  cube = SudoCube((4, 3))
+  cube = SudoCube((2, 3))
   print cube.symbolSize
+  cube.printCube()
+  cube.solve()
   cube.printCube()
